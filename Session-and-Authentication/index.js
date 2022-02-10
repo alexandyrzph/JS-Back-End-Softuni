59db5e1eb34d07a77674162f43877b17524d1820
alexandyrzph <alexandyrzph@gmail.com>
2022-02-10 14:02:02 +0200

    Cookie demo with user

--- Session-and-Authentication/index.js
+++ Session-and-Authentication/index.js
@@ -2,15 +2,32 @@ const express = require('express');
 
 const app = express();
 
-let visited = 0;
+const sessions = {};
 
 app.get('/', (req, res) => {
-    if (req.headers.cookie) {
-        res.send(`Welcome to the site! Site visited ${visited} times!`);
-        ++visited;
+    const cookies = (req.headers.cookie || '')
+        .split(';')
+        .map(c => c.trim())
+        .map(c => c.split('='))
+        .reduce((a, [k, v]) => Object.assign(a, { [k]: v }), {});
+
+    console.log('>>>', cookies);
+
+    let user = sessions[cookies.sessionId];
+
+    if (user == undefined) {
+        const newId = ('000000' + (Math.random() * 999999).toString(16)).slice(-6);
+        user = {
+            visited: 1
+        };
+        sessions[newId] = user;
+
+        res.setHeader('Set-Cookie', `sessionId=${newId}; httpOnly`);
+    } else {
+        user.visited++;
     }
-    res.setHeader('Set-Cookie', `visited=${visited}`);
-    res.send(`Welcome to the site! You are the first visitor!`);
+
+    res.send(`<p>Hello<p><p>You have visited the page ${user.visited} times</p>`);
 });
 
 app.listen(3000, () => console.log('Server started on port 3000!'));
