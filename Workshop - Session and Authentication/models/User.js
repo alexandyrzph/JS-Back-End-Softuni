6192610b4e8850b2a3419e9c5c9e67165d62753d
alexandyrzph <alexandyrzph@gmail.com>
2022-02-11 18:11:02 +0200

    Cheking if password is modified

--- Workshop - Session and Authentication/models/User.js
+++ Workshop - Session and Authentication/models/User.js
@@ -11,8 +11,12 @@ userSchema.methods.comparePassword = async function (password) {
     return await comparePassword(password, this.hashedPassword);
 }
 
-userSchema.pre('save', async function () {
-    this.hashedPassword = await hashPassword(this.hashedPassword);
+userSchema.pre('save', async function (next) {
+    if (this.isModified('hashedPassword')) {
+        this.hashedPassword = await hashPassword(this.hashedPassword);
+    }
+
+    next();
 });
 
 const User = model('User', userSchema);
