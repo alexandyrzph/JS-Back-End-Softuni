911754e8ee58407c71a1d05ee18874469c7bd596
alexandyrzph <alexandyrzph@gmail.com>
2022-02-25 01:17:33 +0200

    Added model validations

--- Exam-Preparation-SharedTrips/controllers/detailsController.js
+++ Exam-Preparation-SharedTrips/controllers/detailsController.js
@@ -1,12 +1,19 @@
 const preload = require('../middleware/preload');
-const { getTripById } = require('../services/tripService');
 
 const router = require('express').Router();
 
 router.get('/details/:id', preload(true), (req, res) => {
+    const trip = res.locals.trip;
+    trip.remainingSeats = Number(trip.seats) - Number(trip.buddies.length);
+
     if (req.session.user) {
-        res.locals.trip.hasUser = true;
-        res.locals.trip.isOwner = req.session.user._id == res.locals.trip.owner._id;
+        trip.hasUser = true;
+        trip.isOwner = req.session.user._id == trip.owner._id;
+
+        if (trip.buddies.some(b => b._id == req.session.user._id)) {
+            trip.isJoined = true;
+        }
+
     }
     res.render('details', { title: 'Details Page' });
 });
